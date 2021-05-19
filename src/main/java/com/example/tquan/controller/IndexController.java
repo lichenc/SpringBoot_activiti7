82ab56f903488a55cc0bcaf058ebe74e8236fc43
lichenc <807060464@qq.com>
2021-05-19 10:57:59 +0800

    项目初始化

---
+++ src/main/java/com/example/tquan/controller/IndexController.java
@@ -0,0 +1,26 @@
+package com.example.tquan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 页面访问控制
+ * Created by chenjin on 2021/5/18 10:21
+ */
+@Controller
+public class IndexController {
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "loginPage";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "registerPage";
+    }
+}
---
+++ src/test/java/com/example/tquan/TquanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tquan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TquanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
